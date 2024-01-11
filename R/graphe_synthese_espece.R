c12aa480da65802594fc4927596128126ba02d6d
CedricMondy <cedric.mondy@gmail.com>
2024-01-11 17:35:45 +0100

    ajoute les données de la station sélectionnée au graphe

--- R/graphe_synthese_espece.R
+++ R/graphe_synthese_espece.R
@@ -9,12 +9,12 @@
 #' @export
 #'
 #' @examples
-graphe_synthese_espece <- function(captures, bassins, departements, espece) {
+graphe_synthese_espece <- function(captures, bassins, departements, espece, station) {
     data_graphe <- captures %>% 
         dplyr::filter(
             dh_libelle %in% bassins,
             dept_id %in% departements
-            ) %>% 
+            ) %>%
         dplyr::group_by(pop_id, annee, ope_id) %>% 
         dplyr::summarise(
             effectif = sum(effectif[esp_code_alternatif == espece]),
@@ -25,10 +25,10 @@ graphe_synthese_espece <- function(captures, bassins, departements, espece) {
     labels_metriques <- c(
         nb_pop = "Nombre de stations",
         eff_tot = "Effectif total sur l'ensemble des stations",
-        eff_moy = "Effectif moyen par station où l'espèce est présente"
+        eff_moy = "Effectif moyen lorsque l'espèce est présente"
     )
     
-    data_graphe %>% 
+    data_n_stations <- data_graphe %>% 
         dplyr::group_by(annee, presence) %>% 
         dplyr::summarise(
             nb_pop = dplyr::n_distinct(pop_id),
@@ -47,27 +47,85 @@ graphe_synthese_espece <- function(captures, bassins, departements, espece) {
         dplyr::mutate(
             metrique = labels_metriques[metrique] %>% 
                 factor(levels = labels_metriques)
+        )
+    
+    data_station <- data_graphe %>% 
+        dplyr::filter(pop_id %in% station) %>% 
+        dplyr::group_by(annee, presence) %>% 
+        dplyr::summarise(
+            eff_moy = mean(effectif),
+            .groups = "drop"
         ) %>% 
-        ggplot2::ggplot() +
-        ggplot2::geom_col(
+        tidyr::pivot_longer(
+            cols = eff_moy,
+            names_to = "metrique",
+            values_to = "valeurs"
+        ) %>% 
+        dplyr::mutate(
+            metrique = labels_metriques[metrique] %>% 
+                factor(levels = labels_metriques),
+            pop_id = station
+        ) %>% 
+        (function(df) {
+            if (nrow(df) > 0) {
+                df %>% 
+                    dplyr::left_join(
+                        captures %>% 
+                            dplyr::filter(pop_id %in% station) %>% 
+                            dplyr::distinct(pop_id, pop_libelle) %>% 
+                            dplyr::mutate(
+                                dplyr::across(
+                                    dplyr::everything(), 
+                                    as.character
+                                    )
+                                ),
+                        by = "pop_id"
+                        )
+                } else {
+                df %>% 
+                        dplyr::mutate(
+                            pop_id = "",
+                            pop_libelle = ""
+                        )
+            }
+        }) %>% 
+        dplyr::mutate(label = paste0(pop_libelle, "\ (", pop_id, ")"))
+        
+    
+    ggplot2::ggplot(
             mapping = ggplot2::aes(
-                x = annee, y = valeurs, fill = presence
-            ),
+                x = annee, y = valeurs
+            )
+        ) +
+        ggplot2::geom_col(
+            data = data_n_stations,
+            mapping = ggplot2::aes(fill = presence),
             colour = "darkgrey"
         ) +
+        ggplot2::geom_point(
+            data = data_station,
+            mapping = ggplot2::aes(colour = label)
+            ) +
         ggplot2::facet_wrap(facets = ggplot2::vars(metrique), ncol = 1, scales = "free_y") +
         ggplot2::scale_fill_manual(
             values = c("white", "grey"),
             labels = c("absence", "présence")
         ) +
+        ggplot2::scale_colour_manual(
+            values = "black"
+        ) +
         ggplot2::labs(
             title = paste0("Evolution temporelle des prises de l'espèce ", espece),
-            x = "", y = "", fill = ""
+            x = "", y = "", fill = "", colour = ""
+        ) +
+        ggplot2::guides(
+            fill = ggplot2::guide_legend(order = 1),
+            color = ggplot2::guide_legend(order = 2)
         ) +
         ggplot2::theme_minimal() +
         ggplot2::theme(
-            legend.position = "right",
-            legend.justification = "top",
+            legend.position = "bottom",
+            legend.justification = "left",
             panel.grid.major.x = ggplot2::element_blank(),
             panel.grid.minor.x = ggplot2::element_blank(),
             panel.grid.minor.y = ggplot2::element_blank(),
